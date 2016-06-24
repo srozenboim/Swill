b5270515cda9b3d578c449ebec428fb056c1578e
Shirley Rozenboim <srozenboim@gmail.com>
2016-06-24 13:32:50 -0700

    Added a list of ingredients to the home page

--- Swill/index.ios.js
+++ Swill/index.ios.js
@@ -9,45 +9,106 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
 } from 'react-native';
 
+var REQUEST_URL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
 class Swill extends Component {
+  constructor(props) {
+    console.log("constructor")
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData)
+
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.drinks),
+          loaded: true,
+        });
+      })
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.title}>
+          Welcome to Swill!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderCategory}
+          style={styles.listView}
+        />
+      </View>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading categories...
         </Text>
       </View>
     );
   }
+
+  renderCategory(category) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.category}>
+          <Text style={styles.title}>{category.strIngredient1}</Text>
+        </View>
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  ListView: {
+    paddingTop: 20,
+    backgroundColor: 'pink',
   },
+  category: {
+    flex: 1,
+  }
 });
 
 AppRegistry.registerComponent('Swill', () => Swill);
