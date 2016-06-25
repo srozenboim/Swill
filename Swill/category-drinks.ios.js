913ed50ba18f51c021563e537d285096456f2b96
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2016-06-24 20:30:59 -0700

    add 1 second animation transition

--- Swill/category-drinks.ios.js
+++ Swill/category-drinks.ios.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 
 import Recipe from './recipe'
+import Spinner from 'react-native-spinkit'
 
 var REQUEST_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
 
@@ -15,10 +16,12 @@ class Category extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      type: 'ChasingDots',
       dataSource: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
       }),
       loaded: false,
+      isVisible: true,
     };
   }
 
@@ -29,6 +32,13 @@ console.log("constructor")
   componentDidMount() {
     console.log("constructor")
     this.fetchData();
+    setTimeout(function(){
+      this.setState({
+        loaded: true,
+        isVisible: false,
+      })
+    }.bind(this), 1000);
+
   }
 
   fetchData() {
@@ -40,7 +50,6 @@ console.log("constructor")
 
         this.setState({
           dataSource: this.state.dataSource.cloneWithRows(responseData.drinks),
-          loaded: true,
         });
       })
       .done();
@@ -69,12 +78,14 @@ console.log("constructor")
     );
   }
 
+
+        // <Text>
+        //   Loading drinks...
+        // </Text>
   renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text>
-          Loading drinks...
-        </Text>
+        <Spinner style={styles.spinner} isVisible={this.state.isVisible} size={100} type={this.state.type} color={'#ff8c00'}/>
       </View>
     );
   }
@@ -92,6 +103,9 @@ console.log("constructor")
 }
 
 const styles = StyleSheet.create({
+  spinner: {
+    marginBottom: 50
+  },
   container: {
     flex: 1,
     alignItems: 'stretch',
